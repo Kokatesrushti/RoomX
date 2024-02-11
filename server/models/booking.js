a4149e3a0b827d908cf100414fae391a7431bd30
imranpathan8107 <imran.pathan.8202@gmail.com>
2024-02-11 23:34:44 +0530

    commit

--- server/models/booking.js
+++ server/models/booking.js
@@ -3,28 +3,35 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BookingSchema = new Schema({
-    room: {
-        type: Schema.Types.ObjectId,
-        ref: 'Room',
-        required: true
-    },
-    users: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    startDate: {
-        type: Date, 
-    },
-    endDate: {
-        type: Date, 
-    },
-    startTime: {
-        type: Date, // For hourly bookings
-    },
-    endTime: {
-        type: Date, 
-    }
+  roomId: {
+    type: Schema.Types.ObjectId,
+    ref: "Room",
+    required: true,
+  },
+  provider_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Provider",
+    required: true,
+  },
+  users: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    // required: true,
+  },
+  startDate: {
+    type: Date,
+  },
+  endDate: {
+    type: Date,
+  },
+  startTime: {
+    type: Date, // For hourly bookings
+    required: false,
+  },
+  endTime: {
+    type: Date,
+    required: false,
+  },
 });
 
 const BookingModel = mongoose.model("Booking", BookingSchema);
